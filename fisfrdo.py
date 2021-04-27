024e2df8791fbdba50be6bd7f6357db1131e0b64
Alexander Andreev <andreev-alef@users.noreply.github.com>
2021-04-27 17:39:40 +0700

    Чтение файла с удостоверениями

--- fisfrdo.py
+++ fisfrdo.py
@@ -2,12 +2,12 @@ import openpyxl
 
 wb = openpyxl.load_workbook(filename="openpyxl-удостоверения.xlsx", data_only=True)
 ws = wb.active
-print(ws["e3456"].value,
-      ws["f3456"].value,
-      ws["g3456"].value,
-      ws["i3456"].value,
-      ws["j3456"].value,
-      ws["k3456"].value,
-      ws["L3456"].value,
-      ws["M3456"].value,
-      ws["n3456"].value)
+print(ws["e3456"].value)
+print(ws["f3456"].value)
+print(ws["g3456"].value)
+print(ws["i3456"].value)
+print(ws["j3456"].value)
+print(ws["k3456"].value)
+print(ws["L3456"].value)
+print(ws["M3456"].value)
+print(ws["n3456"].value)
