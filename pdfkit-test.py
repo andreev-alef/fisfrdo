f2bcd458159d2974ca9e338a7e5f5e60fa8031b2
Alexander Andreev <andreev-alef@users.noreply.github.com>
2021-05-14 15:56:08 +0700

    Добавление стилей и изображения в PDF документ

--- pdfkit-test.py
+++ pdfkit-test.py
@@ -1,17 +1,27 @@
 import pdfkit
-
+#    <meta charset="utf-8" />
 html_str = '''
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="utf-8" />
+
 </head>
 <body>
     <p><span style="color: #000000; font-family: Exo2; font-size: 16pt;"><b>Проба пера</b>. Синий</span></p>
     <p><span style="color: #ff0000; font-family: 'Noto Serif'; font-size: 16pt;"><b>Проба пера №2</b></span><br />
     Разрыв строки
     </p>
+    <p><img src="http://192.168.0.30:8888/fisfrdo/logo.jpg" style="width: 100mm" /></p>
 </body>
 </html>
 '''
-pdfkit.from_string(html_str, 'data.pdf')
+
+ops = {
+    'page-size': 'Letter',
+    'margin-top': '10mm',
+    'margin-right': '10mm',
+    'margin-bottom': '10mm',
+    'margin-left': '10mm',
+    'encoding': "UTF-8",
+    'orientation':'Landscape'}
+pdfkit.from_string(html_str, 'data.pdf', options=ops)
